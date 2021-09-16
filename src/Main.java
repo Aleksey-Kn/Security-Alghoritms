ba31374dbf6e8af5ba38895f86de88ba2dfba279
Кожемякин Алексей Андреевич <aleksey.k2000@mail.ru>
2021-09-16 10:39:58 +0700

    Add parsing exception

--- src/Main.java
+++ src/Main.java
@@ -12,57 +12,61 @@ public class Main {
         Coder first, second;
         boolean flag = true;
         while (flag) {
-            System.out.println("Выберите алгоритм");
+            System.out.println("Выберите номер алгоритма одной цифрой без дополнительных символов");
             System.out.println("1. Быстрое возведение в степень по модулю");
             System.out.println("2. Обобщенный алгоритм Евклида");
             System.out.println("3. Построение общего ключа для двух абонентов по схеме Диффи-Хеллмана");
             System.out.println("4. Нахождение дискретного логарифма при помощи алгоритма «Шаг младенца, шаг великана»");
-            switch (scanner.nextInt()) {
-                case 1:
-                    System.out.println("1. Данные из файла\n2. Случайные числа");
-                    if (scanner.nextInt() == 1) {
-                        data = readFile(scanner);
-                    } else
-                        data = new int[]{random(), random(), random()};
-                    System.out.printf("a = %d, x = %d, b = %d\n", data[0], data[1], data[2]);
-                    result = SpecialMath.powOnModule(data[0], data[1], data[2]);
-                    System.out.println("Mod: " + result);
-                    break;
-                case 2:
-                    System.out.println("1. Данные из файла\n2. Случайные числа");
-                    if (scanner.nextInt() == 1) {
-                        data = readFile(scanner);
-                    } else
-                        data = new int[]{random(), random()};
-                    System.out.printf("a = %d, b = %d\n", data[0], data[1]);
-                    result = SpecialMath.nod(data[0], data[1]);
-                    System.out.println("НОД: " + result);
-                    break;
-                case 3:
-                    first = new Coder();
-                    second = new Coder();
-                    result = SpecialMath.diffihellman(first);
-                    result2 = SpecialMath.diffihellman(second);
-                    System.out.println("Кодовое число первого абонента: " + first.apply(result2));
-                    System.out.println("Кодовое число второго абонента: " + second.apply(result));
-                    break;
-                case 4:
-                    System.out.println("1. Данные из файла\n2. Случайные числа");
-                    if (scanner.nextInt() == 1) {
-                        data = readFile(scanner);
-                    } else {
-                        data = new int[]{random(), SieveEratosfen.getInstance().randomPrime(), random()};
-                        while (data[2] >= data[1] || SpecialMath.nod(data[1], data[2]) != 1){
-                            data[1] = SieveEratosfen.getInstance().randomPrime();
-                            data[2] = random();
+            try {
+                switch (Integer.parseInt(scanner.next())) {
+                    case 1:
+                        System.out.println("1. Данные из файла\n2. Случайные числа");
+                        if (scanner.nextInt() == 1) {
+                            data = readFile(scanner);
+                        } else
+                            data = new int[]{random(), random(), random()};
+                        System.out.printf("a = %d, x = %d, b = %d\n", data[0], data[1], data[2]);
+                        result = SpecialMath.powOnModule(data[0], data[1], data[2]);
+                        System.out.println("Mod: " + result);
+                        break;
+                    case 2:
+                        System.out.println("1. Данные из файла\n2. Случайные числа");
+                        if (scanner.nextInt() == 1) {
+                            data = readFile(scanner);
+                        } else
+                            data = new int[]{random(), random()};
+                        System.out.printf("a = %d, b = %d\n", data[0], data[1]);
+                        result = SpecialMath.nod(data[0], data[1]);
+                        System.out.println("НОД: " + result);
+                        break;
+                    case 3:
+                        first = new Coder();
+                        second = new Coder();
+                        result = SpecialMath.diffihellman(first);
+                        result2 = SpecialMath.diffihellman(second);
+                        System.out.println("Кодовое число первого абонента: " + first.apply(result2));
+                        System.out.println("Кодовое число второго абонента: " + second.apply(result));
+                        break;
+                    case 4:
+                        System.out.println("1. Данные из файла\n2. Случайные числа");
+                        if (scanner.nextInt() == 1) {
+                            data = readFile(scanner);
+                        } else {
+                            data = new int[]{random(), SieveEratosfen.getInstance().randomPrime(), random()};
+                            while (data[2] >= data[1] || SpecialMath.nod(data[1], data[2]) != 1) {
+                                data[1] = SieveEratosfen.getInstance().randomPrime();
+                                data[2] = random();
+                            }
                         }
-                    }
-                    System.out.printf("a = %d, p = %d, y = %d\n", data[0], data[1], data[2]);
-                    result = SpecialMath.steps(data[0], data[1], data[2]);
-                    System.out.println("Степень: " + result);
-                    break;
-                default:
-                    flag = false;
+                        System.out.printf("a = %d, p = %d, y = %d\n", data[0], data[1], data[2]);
+                        result = SpecialMath.steps(data[0], data[1], data[2]);
+                        System.out.println("Степень: " + result);
+                        break;
+                    default:
+                        flag = false;
+                }
+            } catch (Exception e){
+                System.out.println("\nНекорректный ввод! Вы не слишком умный пользователь!\n");
             }
         }
     }
