4ad17e9ddc4be52849a8f813aa1fc19f54b0bdbc
Кожемякин Алексей Андреевич <aleksey.k2000@mail.ru>
2021-09-27 20:32:00 +0700

    Realise Shamir cipher

--- src/shifrs/Shamir.java
+++ src/shifrs/Shamir.java
@@ -6,18 +6,34 @@ import baseAlghoritms.SpecialMath;
 import java.util.Random;
 
 public class Shamir {
-    private int c, d;
+    private int c, d, p;
 
     public Shamir(){
         Random random = new Random();
-        int p = Environ.getInstance().getP();
+        p = Environ.getInstance().getP();
         int[] t;
         do {
-            c = random.nextInt();
-            t = SpecialMath.nod(c, p - 1);
-        } while (t[0] != 1);
-        d = t[1];
+            do {
+                c = Math.abs(random.nextInt()) % (p - 1) + 1;
+                t = SpecialMath.nod(c, p - 1);
+            } while (t[0] != 1);
+            d = t[1];
+        } while (d <= 0 || d >= p);
     }
 
+    public int encoding(byte m){
+        return SpecialMath.powOnModule(m, c, p);
+    }
 
+    public int generateX2(int x1){
+        return SpecialMath.powOnModule(x1, c, p);
+    }
+
+    public int generateX3(int x2){
+        return SpecialMath.powOnModule(x2, d, p);
+    }
+
+    public byte decoding(int x3){
+        return (byte) SpecialMath.powOnModule(x3, d, p);
+    }
 }
